c59caa53788e3b7fb05c2f8dc1e5071731944102
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-13 14:08:41 +0800

    Add files via upload

---
+++ Selection_Sort.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+void Selection_Sort(int a[],int n) {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int temp = i;
+		for (int j = i; j < n; j++) {
+			if (a[temp] > a[j])temp = j;
+		}
+		swap(a[temp], a[i]);
+		cnt++;
+	}for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl << cnt;
+	
+
+
+}
+int main() {
+	int n;
+	cin >> n;
+	int T = n;
+	int a[1000];
+	
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	Selection_Sort(a, n);
+	return 0;
+}
