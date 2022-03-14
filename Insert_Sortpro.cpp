5a5c5e82250136f88151c2d78d706f2eb74d665f
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-14 17:07:38 +0800

    Add files via upload

---
+++ Insert_Sortpro.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+using namespace std;
+int binary_search(int a[], int n, int target) {
+    int left = 0, right = n - 1;
+    int mid;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        if (target < a[mid]) {
+            right = mid - 1;
+        }
+
+        else if (target > a[mid]) {
+            left = mid + 1;
+        }
+        else return mid;
+    }
+    return left;
+
+}
+int main() {
+
+    int n;
+    cin >> n;
+    int a[1000] = { };
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[i - 1]) {
+            
+            int temp = binary_search(a, i, a[i]);
+            int temp1 = a[i];
+            for (int j = i; j >temp; j--) {
+                a[j] = a[j-1];
+            }
+            a[temp] = temp1; //插入元素;
+           
+        }
+    }
+   
+    
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
